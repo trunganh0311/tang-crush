bb34c7f3105d4d62b23199c7e4e83d804969822b
trunganh0311 <88879958+trunganh0311@users.noreply.github.com>
2021-08-13 15:57:45 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,15 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
-}
+    introTitle: 'Babi uiii',
+    introDesc: `Anh hỏi babi một câu nha`,
+    btnIntro: 'Anh hỏi đi',
+    title: 'Babi nghe nè',
+    desc: 'Sao babi iu tui nhiều z 🥰 ',
+    btnYes: 'Tại vì anh dâm nè =))',
+    btnNo: 'Tại anh đẹp á :3',
+    question:'Tự nhiên yêu ngta vì dâm, có dâm âu, đẹp mà',
+    btnReply: 'Buồn òi',
+    reply: 'Iu nhờ đẹp mà 🥺 ',
+    mess: 'Dạ iu babi nhìu lém',
+    messDesc: 'Thưn bé iu lắm ó',
+    btnAccept: 'Dạaaaa 😘',}
+ 
